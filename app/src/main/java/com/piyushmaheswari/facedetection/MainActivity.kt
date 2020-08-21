392e5720ee4b8817e8be9d84b87c1f22564ec489
Piyush Maheswari <piyush.maheswari@testbook.com>
2020-08-22 00:18:36 +0530

    Added Camera and Gallery functionality

--- app/src/main/java/com/piyushmaheswari/facedetection/MainActivity.kt
+++ app/src/main/java/com/piyushmaheswari/facedetection/MainActivity.kt
@@ -1,14 +1,58 @@
 package com.piyushmaheswari.facedetection
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    var bitmap: Bitmap?= null
+    lateinit var tempBitmap: Bitmap
+    lateinit var paint: Paint
+    lateinit var canvas: Canvas
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
 
+    fun cameraSelect(view: View) {
+
+        val intent=Intent()
+        intent.action=MediaStore.ACTION_IMAGE_CAPTURE
+        if(intent.resolveActivity(packageManager)!=null)
+        {
+            startActivityForResult(intent, REQUESTCODE2)
+        }
+
+    }
+
+    fun selectImage(view: View) {
+        val intent=Intent()
+        intent.type="image/*"
+        intent.action=Intent.ACTION_GET_CONTENT
+        if(intent.resolveActivity(packageManager)!=null)
+        {
+            startActivityForResult(intent, REQUESTCODE)
+        }
+
+    }
+
+    companion object {
+        const val REQUESTCODE=101
+        const val REQUESTCODE2=102
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
     }
 }
